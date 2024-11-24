2f65d9ff00ca31a58415c7f0ffa8a4448bf8d647
CarlinLambiMia <cego.judo@gmail.com>
2024-11-24 13:39:43 -0400

    tarefa 2

--- tarefa 2/script.js
+++ tarefa 2/script.js
@@ -1,13 +1,15 @@
-const miniaturas = document.querySelectorAll('.miniatura');
-const imgAmpliada = document.querySelector('#imgAmpliada');
+const miniaturas = document.querySelectorAll('.miniatura')
+const imgAmpliada = document.querySelector('#imgAmpliada')
 
-miniaturas.forEach(miniatura => {
-    miniatura.addEventListener('click', () => {
-        miniatura.forEach(miniatura => {
-            miniatura.style.opacity = '0,4'
+miniaturas.forEach(miniatura =>{
+        miniatura.addEventListener('click', ()=>{
+
+            miniaturas.forEach(miniatura =>{
+                miniatura.style.opacity= '0.4'
+            })
+
+            imgAmpliada.src = miniatura.src
+            imgAmpliada.style.display= 'block'
+            miniatura.style.opacity= '1'
         })
-        imgAmpliada.src = miniatura.src;
-        imgAmpliada.style.display = 'block';
-        miniatura.style.opacity = '1';
     })
-})
