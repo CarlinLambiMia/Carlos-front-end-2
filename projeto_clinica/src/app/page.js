86f1b450784d5d9382773586b0b346e59b3a3742
CarlinLambiMia <cego.judo@gmail.com>
2025-02-12 08:27:05 -0400

    mudar font

--- projeto_clinica/src/app/page.js
+++ projeto_clinica/src/app/page.js
@@ -3,6 +3,9 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.div}>
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+      <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet"></link>
       <div className={styles.campoParagrafo}>
         <h1> Bem-vindo á clínica Bruxelas</h1>
         <p> Nossa equipe de profissionais altamente qualificados está pronta para cuidar da sua saúde
--- projeto_clinica/src/componentes/Header/index.js
+++ projeto_clinica/src/componentes/Header/index.js
@@ -7,6 +7,9 @@ import Image from "next/image"
 export default function Header() {
     return (
         <div className={styles.header}>
+            <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+            <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet"></link>
             <nav className={styles.navegacao}>
                 <div>
                 <Image src={'/images/logo.png'} width={70} height={70} alt="Logo" className={styles.logo}/>
@@ -23,16 +26,16 @@ export default function Header() {
                         <Link href="#" className={styles.opcaoHeader}>Médicos</Link>
                         <ul className={styles.Submenu}>
                             <li className={styles.headerLi}>
-                                <button><Link href="/medicos" className={styles.opcaoHeader}>Listar médicos</Link></button>
+                               <Link href="/medicos" className={styles.opcaoHeader}>Listar médicos</Link>
                             </li>
                             <li className={styles.headerLi}>
-                                <button><Link href="#" className={styles.opcaoHeader}>Adicionar</Link></button>
+                                <Link href="#" className={styles.opcaoHeader}>Adicionar</Link>
                             </li>
                             <li className={styles.headerLi}>
-                                <button><Link href="#" className={styles.opcaoHeader}>Editar</Link></button>
+                                <Link href="#" className={styles.opcaoHeader}>Editar</Link>
                             </li>
                             <li className={styles.headerLi}>
-                                <button><Link href="#" className={styles.opcaoHeader}>Excluir</Link></button>
+                               <Link href="#" className={styles.opcaoHeader}>Excluir</Link>
                             </li>
                         </ul>
                     </li>
@@ -40,16 +43,16 @@ export default function Header() {
                         <Link href="#" className={styles.opcaoHeader}>Pacientes</Link>
                         <ul className={styles.Submenu}>
                             <li className={styles.headerLi}>
-                                <button><Link href="/pacientes" className={styles.opcaoHeader}>Listar pacientes</Link></button>
+                                <Link href="/pacientes" className={styles.opcaoHeader}>Listar pacientes</Link>
                             </li>
                             <li className={styles.headerLi}>
-                                <button><Link href="#" className={styles.opcaoHeader}>Adicionar</Link></button>
+                                <Link href="#" className={styles.opcaoHeader}>Adicionar</Link>
                             </li>
                             <li className={styles.headerLi}>
-                                <button><Link href="#" className={styles.opcaoHeader}>Editar</Link></button>
+                                <Link href="#" className={styles.opcaoHeader}>Editar</Link>
                             </li>
                             <li className={styles.headerLi}>
-                                <button><Link href="#" className={styles.opcaoHeader}>Excluir</Link></button>
+                                <Link href="#" className={styles.opcaoHeader}>Excluir</Link>
                             </li>
                         </ul>
                     </li>
@@ -57,16 +60,16 @@ export default function Header() {
                         <Link href="#" className={styles.opcaoHeader}>Agaendamento</Link>
                         <ul className={styles.Submenu}>
                             <li className={styles.headerLi}>
-                                <button><Link href="/agendamentos" className={styles.opcaoHeader}>Listar Consultas</Link></button>
+                               <Link href="/agendamentos" className={styles.opcaoHeader}>Listar Consultas</Link>
                             </li>
                             <li className={styles.headerLi}>
-                                <button><Link href="#" className={styles.opcaoHeader}>Agendar Consulta</Link></button>
+                                <Link href="#" className={styles.opcaoHeader}>Agendar Consulta</Link>
                             </li>
                             <li className={styles.headerLi}>
-                                <button><Link href="#" className={styles.opcaoHeader}>Editar Agendamento</Link></button>
+                                <Link href="#" className={styles.opcaoHeader}>Editar Agendamento</Link>
                             </li>
-                            <li className={styles.headerLi}>
-                                <button><Link href="#" className={styles.opcaoHeader}>Cancelar</Link></button>
+                            <li className={styles.headerLi}> 
+                                <Link href="#" className={styles.opcaoHeader}>Cancelar</Link>
                             </li>
                         </ul>
                     </li>
